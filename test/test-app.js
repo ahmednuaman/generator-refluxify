e00d95931cde0f9dafeaef2a7c85df5b7e62e0f8
Ahmed Nuaman <ahmed@ahmednuaman.com>
2015-01-01 13:17:19 +0000

    sorted tests

--- test/test-app.js
+++ test/test-app.js
@@ -8,15 +8,33 @@ describe('refluxify:app', function () {
     helpers.run(path.join(__dirname, '../app'))
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
-      .on('method', function (method) {
-        console.log(method);
-      })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'package.json'
+      '__tests__/spec/views/main.spec.js',
+      '__tests__/support/preprocessor.js',
+      'assets/jsx/app.jsx',
+      'assets/jsx/views/main.jsx',
+      'assets/less/app.less',
+      'assets/less/global.less',
+      'gulp/__common__.js',
+      'gulp/browserify.js',
+      'gulp/build.js',
+      'gulp/clean.js',
+      'gulp/cssmin.js',
+      'gulp/default.js',
+      'gulp/jest.js',
+      'gulp/jscs.js',
+      'gulp/less.js',
+      'gulp/uglify.js',
+      '.gitignore',
+      '.jscsrc',
+      'gulpfile.js',
+      'index.html',
+      'package.json',
+      'server.js'
     ]);
   });
 });
